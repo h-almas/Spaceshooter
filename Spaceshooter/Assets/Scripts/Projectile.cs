9f62ac9ed2042858a741d633f1bec94e0bcdabbf
h-almas <80386025+h-almas@users.noreply.github.com>
2023-01-21 14:55:02 +0100

    cleaning up projectile code

--- Spaceshooter/Assets/Scripts/Projectile.cs
+++ Spaceshooter/Assets/Scripts/Projectile.cs
@@ -1,47 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float projectileSpeed;
-    private Camera _mainCamera;
-    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _mainCamera = Camera.main;
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float amtToMove = projectileSpeed * Time.deltaTime;
         transform.Translate(Vector3.up * amtToMove);
-        
-        Vector3 positionInViewSpace = _mainCamera.WorldToViewportPoint(transform.position);
-        
-        if (positionInViewSpace.y > 1.2f)
-        {
-            Destroy(gameObject);
-        }
-
     }
-    
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
             Debug.Log("We hit " + other.name);
             Player.Score += other.GetComponent<Enemy>().GetPower();
-            
             Destroy(gameObject);
         }
-
-        
     }
 }
