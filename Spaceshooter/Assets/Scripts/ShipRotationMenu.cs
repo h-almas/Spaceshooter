253c009bde16ac813d80cbb4b6c05a665eb528b9
Rob3rtBr343 <116502158+Rob3rtBr343@users.noreply.github.com>
2023-01-24 19:56:21 +0100

    Almost finished Main Menu

--- Spaceshooter/Assets/Scripts/ShipRotationMenu.cs
+++ Spaceshooter/Assets/Scripts/ShipRotationMenu.cs
@@ -13,6 +13,6 @@ public class ShipRotationMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,45 * Time.deltaTime);
+        transform.Rotate(0,45 * Time.deltaTime,0);
     }
 }
