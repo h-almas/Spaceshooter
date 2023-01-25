b4d42a9048328f657a7946e3a500cee8af5d7764
Rob3rtBr343 <116502158+Rob3rtBr343@users.noreply.github.com>
2023-01-25 17:50:39 +0100

    Polished UI in all scenes and started working on intermission menu

--- Spaceshooter/Assets/Scripts/Player Scripts/PlayerBonus.cs
+++ Spaceshooter/Assets/Scripts/Player Scripts/PlayerBonus.cs
@@ -15,6 +15,8 @@ public class PlayerBonus : MonoBehaviour
 
     public TMPro.TextMeshProUGUI scoreText;
     public TMPro.TextMeshProUGUI starsText;
+    public TMPro.TextMeshProUGUI scoreTextFinal;
+    public TMPro.TextMeshProUGUI starsTextFinal;
     
     
     // Start is called before the first frame update
@@ -43,5 +45,7 @@ public class PlayerBonus : MonoBehaviour
 
         scoreText.text = "Score: " + PlayerPrefs.GetInt("Score", 0);
         starsText.text = "Stars: " + stars;
+        scoreTextFinal.text = "SCORE: " + PlayerPrefs.GetInt("Score", 0);
+        starsTextFinal.text = "STARS: " + stars;
     }
 }
