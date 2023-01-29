dd7d34971824a3e79aeef3c6fb64b2fa62c1c539
h-almas <80386025+h-almas@users.noreply.github.com>
2023-01-29 03:48:08 +0100

    tried adding some visuals for powerups so we can discern them

--- Spaceshooter/Assets/Scripts/Weapons/WeaponCannon.cs
+++ Spaceshooter/Assets/Scripts/Weapons/WeaponCannon.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class WeaponCannon : Weapon
 {
+    public GameObject cannonBall;
+    
     public override void Shoot()
     {
         if (timeSinceLastShot >= timeBetweenShots)
         {
             if (Input.GetKey(KeyCode.Space)){
                 // TODO: Should be more ball like but this will do for now
-                GameObject projectile = Instantiate(projectilePrefab, weaponTransform.position, Quaternion.identity);
+                GameObject projectile = Instantiate(cannonBall, weaponTransform.position, Quaternion.identity);
                 projectile.transform.localScale *= 8; 
                 timeSinceLastShot = 0;
             }
