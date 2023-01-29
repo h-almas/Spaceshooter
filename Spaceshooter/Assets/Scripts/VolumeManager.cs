56e526d53fc37e59587285dc5535b2c48c321f8a
Rob3rtBr343 <116502158+Rob3rtBr343@users.noreply.github.com>
2023-01-29 23:53:54 +0100

    Added music volume manager

--- Spaceshooter/Assets/Scripts/VolumeManager.cs
+++ Spaceshooter/Assets/Scripts/VolumeManager.cs
@@ -1,15 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider volumeSlider;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume",-6);
+            LoadSettings();
+        }
+        else
+        {
+            LoadSettings();
+        }
+    }
 
     public void ChangeMusicVolume(float musicVolume)
     {
-        audioMixer.SetFloat("musicVolume", Mathf.Log10(musicVolume) * 20);
+        audioMixer.SetFloat("musicVolume", musicVolume);
+        SaveSettings();
     }
 
+    private void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+    private void LoadSettings()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
 }
