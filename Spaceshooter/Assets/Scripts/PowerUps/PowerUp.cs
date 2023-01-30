87a57023c3a92590bbe835f47c24bdc799a81e72
h-almas <80386025+h-almas@users.noreply.github.com>
2023-01-30 18:56:01 +0100

    Added rarities to PowerUps

--- Spaceshooter/Assets/Scripts/PowerUps/PowerUp.cs
+++ Spaceshooter/Assets/Scripts/PowerUps/PowerUp.cs
@@ -7,6 +7,7 @@ public class PowerUp : MonoBehaviour
     [SerializeField] private float speed = 1.5f;
     [SerializeField] protected float duration = 5f;
     [SerializeField] protected string message;
+    [SerializeField] public float rarity = 1;
     
     private void Update()
     {
