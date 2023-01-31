65f741e4ab8fc88580e9242b266790c0d28f7000
h-almas <80386025+h-almas@users.noreply.github.com>
2023-01-31 20:29:32 +0100

    Half fixing bonuslevel bounds and sliiightly buffing homingprojectile

--- Spaceshooter/Assets/Scripts/Player Scripts/PlayerBonus.cs
+++ Spaceshooter/Assets/Scripts/Player Scripts/PlayerBonus.cs
@@ -11,17 +11,20 @@ public class PlayerBonus : MonoBehaviour
     [SerializeField] private Vector2 tilt;
     [SerializeField] private GameObject projectile;
     [SerializeField] private Transform weaponLocation;
-
+    private Camera mainCamera;
+    
     public TMPro.TextMeshProUGUI scoreText;
     public TMPro.TextMeshProUGUI starsText;
     public TMPro.TextMeshProUGUI scoreTextFinal;
     public TMPro.TextMeshProUGUI starsTextFinal;
     
     
+    
     // Start is called before the first frame update
     void Start()
     {
         Player.Score = 0;
+        mainCamera = Camera.main;
         _initialRotationBL = transform.rotation;
     }
 
@@ -36,7 +39,34 @@ public class PlayerBonus : MonoBehaviour
 
         transform.rotation = Quaternion.Slerp(_initialRotationBL, Quaternion.Euler
             (tilt.x * Input.GetAxis("Vertical"),0, -90), 1);
+        
+        Vector3 positionInViewSpace = mainCamera.WorldToViewportPoint(transform.position);
 
+        Debug.Log(positionInViewSpace);
+        
+        if (positionInViewSpace.x < .0f)
+        {
+            transform.position = 
+                mainCamera.ViewportToWorldPoint(new Vector3(0f, positionInViewSpace.y, positionInViewSpace.z));
+        }
+        else if (positionInViewSpace.x > .35f)
+        {
+            transform.position =
+                mainCamera.ViewportToWorldPoint(new Vector3(.35f, positionInViewSpace.y, positionInViewSpace.z));
+        }
+            
+        if (positionInViewSpace.y < 0f)
+        {
+            transform.position =
+                mainCamera.ViewportToWorldPoint(new Vector3(positionInViewSpace.x, 0f, positionInViewSpace.z));
+        }
+        else if (positionInViewSpace.y > 1f)
+        {
+            transform.position =
+                mainCamera.ViewportToWorldPoint(new Vector3(positionInViewSpace.x, 1f, positionInViewSpace.z));
+        }
+        
+        
         if (Input.GetKeyDown("space"))
         {
             Instantiate(projectile, weaponLocation.position, transform.rotation);
